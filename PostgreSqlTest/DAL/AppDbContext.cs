23123427608b0deda42563d8d50365c819b5e4e2
Rashad133 <tu82907hw@code.edu.az>
2024-05-30 23:00:30 +0400

    Add project files.

---
+++ PostgreSqlTest/DAL/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PostgreSqlTest.Model;
+
+
+namespace PostgreSqlTest.DAL
+{
+    public class AppDbContext:DbContext
+    {
+        private readonly IConfiguration _configuration;
+        public AppDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration; 
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(_configuration.GetConnectionString("TestDB"));
+        }
+
+        public DbSet<Product> Products { get; set; }    
+    }
+}
